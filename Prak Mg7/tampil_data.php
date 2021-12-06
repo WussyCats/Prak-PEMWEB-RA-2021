c6569e9e3f5f404b7a48d3e1b207ef884aa98e22
Muhammad Farisi Zatwara Putra Unyi <82500240+WussyCats@users.noreply.github.com>
2021-12-06 21:19:20 +0700

    Delete Prak Mg7 directory

--- Prak Mg7/tampil_data.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include 'connection.php';
-$id = $_POST['id'];
-$sql = mysqli_query($conn, "SELECT * FROM adventure WHERE adv_level = '$id' ");
-$result = mysqli_fetch_array($sql);
-$result['id'] = $id;
-echo json_encode($result);
-?>
